b16b16e3692cf10c83da4b40a236c590a7ea7792
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2024-11-04 13:05:55 +0700

    finish exercise 5 6 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start !== parseInt(start, 10)  || end !== parseInt(end, 10) || start < 0 || end < 0) 
+        return "ERROR";
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    return ((end - start + 1) * (start + end))/2;
 };
 
 // Do not edit below this line
