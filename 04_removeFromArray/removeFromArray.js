e04125a38caead3213b489a03c5eea8d0db4a8a1
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2024-11-04 10:39:13 +0700

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach((arg) =>{
+        while(array.indexOf(arg) >= 0) {
+            index = array.indexOf(arg);
+            if(index >= 0) array.splice(index, 1);
+        }
+    });
+    return array
 };
 
 // Do not edit below this line
