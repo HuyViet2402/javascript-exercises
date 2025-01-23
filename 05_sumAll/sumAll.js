df48b02fb12df867848175bedfcc97f762979579
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-01-23 03:21:12 +0000

    Done exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(idx1, idx2) {
+    if (!Number.isInteger(idx1) || !Number.isInteger(idx2) || idx1 < 0 || idx2 < 0) {
+        return "ERROR";
+    }
+    let largeNumber = null;
+    let smallNumber = null;
 
+    if(idx1 > idx2) {
+        largeNumber = idx1;
+        smallNumber = idx2;
+    }
+    else {
+        largeNumber = idx2;
+        smallNumber = idx1;
+    }
+    let sum = 0;
+    for(let i = smallNumber; i <= largeNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
