81d34e4638eb9100e64adf69c0e164598a532873
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-01-23 03:12:07 +0000

    Done exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...removeIdx) {
+    answer = [];
+    for (let i = 0; i < list.length; i++) {
+        if (!removeIdx.includes(list[i])) {
+            answer.push(list[i]);
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
