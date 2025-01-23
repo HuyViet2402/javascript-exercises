898f7c015e343594420969ad24e769134337a43e
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-01-23 03:40:12 +0000

    Done exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  Celsius = (Fahrenheit - 32) * 5/9;
+  return Math.round(Celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  Fahrenheit = Celsius * 9/5 + 32;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
