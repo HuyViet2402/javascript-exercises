b210bff1d1259a9eeb955dd331c99f4fcdc4d112
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-02-19 09:40:00 +0000

    Done exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrPeople, oldesAge = 0, oldestName = '') {
+    if (arrPeople.length == 0) { 
+        console.log(oldestName);
+        return oldestName; 
+    }
+    let currentTimeOrDeath = (arrPeople[0].yearOfDeath == undefined)? 2025 : arrPeople[0].yearOfDeath;
+    let currentAge = currentTimeOrDeath - arrPeople[0].yearOfBirth;
+    if(oldesAge < currentAge) {
+        oldesAge =  currentAge;
+        oldestName = arrPeople[0].name;
+    }
+    return findTheOldest(arrPeople.slice(1), oldesAge, oldestName);
 };
 
 // Do not edit below this line
