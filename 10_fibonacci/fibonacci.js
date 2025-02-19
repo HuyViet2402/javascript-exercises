25262d64c9630079afa11596afa4e372de0297bd
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-02-19 09:05:15 +0000

    Done exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let cleanInput = parseInt(input)
+    if(Number.isNaN(cleanInput) || cleanInput < 0) { return "OOPS" }
+    if (cleanInput === 0) {
+        return 0;
+    }
 
+    if (cleanInput === 1) {
+        return 1;
+    }
+
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= cleanInput; i++) {
+        const temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
