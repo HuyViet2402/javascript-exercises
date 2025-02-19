93c8e5ceddabb56b137c35cfb3d8b335a1f5aff6
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-02-19 08:33:18 +0000

    Done exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reverseStr = cleanStr.split("").reverse().join("");
+    return cleanStr == reverseStr;
 };
 
 // Do not edit below this line
